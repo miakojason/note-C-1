ae7752f6f8cba65913d70d892a22902b94347362
miakojason <miakojason@gmail.com>
2024-01-18 00:34:27 +0800

    建立完成9個表加修改db錯誤，顯示成功頁尾版權跟人數

--- back.php
+++ back.php
@@ -74,7 +74,7 @@
 				</div>
 			</div>
 			<div class="di" style="height:540px; border:#999 1px solid; width:76.5%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
-				<!--正中央-->
+				<!--正中央--> 
 				<table width="100%">
 					<tbody>
 						<tr>
