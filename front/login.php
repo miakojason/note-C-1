759094ef016bcc4e9887595365913f55e81f386c
miakojason <miakojason@gmail.com>
2024-01-18 23:02:33 +0800

    努力加油現在時間緊迫加緊利用寒假晚上時間練習，不能鬆懈

--- front/login.php
+++ front/login.php
@@ -9,3 +9,4 @@
 		<p class="cent">密碼 ： <input name="ps" type="password"></p>
 		<p class="cent"><input value="送出" type="submit"><input type="reset" value="清除"></p>
 	</form>
+</div>
--- index.php
+++ index.php
@@ -19,7 +19,6 @@
 			<div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
 		</div>
 	</div>
-	<iframe style="display:none;" name="back" id="back"></iframe>
 	<div id="main">
 		<a title="" href="./home_files/home.htm">
 			<div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div><!--標題-->
@@ -31,12 +30,20 @@
 					<span class="t botli">主選單區</span>
 				</div>
 				<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-					<span class="t">進站總人數 : <?=$Total->find(1)['total'];?>
-</span>
+					<span class="t">進站總人數 : <?= $Total->find(1)['total']; ?>
+					</span>
 				</div>
 			</div>
 			<!-- ------- -->
-		<?php include_once "./front/news.php"?>
+			<?php
+			$do = $_GET['do'] ?? 'main';
+			$file = "./front/{$do}.php";
+			if (file_exists($file)) {
+				include "$file";
+			} else {
+				include "./front/main.php";
+			}
+			?>
 			<!-- ------- -->
 			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 			<script>
@@ -84,7 +91,7 @@
 		</div>
 		<div style="clear:both;"></div>
 		<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-			<span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
+			<span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom']; ?></span>
 		</div>
 	</div>
 
